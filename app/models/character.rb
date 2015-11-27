731d301972bc427e48fe30ea06ef9f03540d9213
NoneOfMaster <rcraigt@gmail.com>
2015-11-27 12:55:29 -0800

    funny GoT pun

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
   
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  #def build_show(show_hash)
+  #  built_show = Show.create(show_hash)
+  #  built_show.character = self
+  #end
+
 end
